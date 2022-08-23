406908fa0395bd26849654c40a345d96fc50a433
MrDragoRozum <roozumaleks@gmail.com>
2022-08-23 12:55:27 +0300

    Реализовано активити для первого макета

--- app/src/main/java/com/example/cafe/MainActivity.java
+++ app/src/main/java/com/example/cafe/MainActivity.java
@@ -4,14 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextName;
+    EditText editTextPassword;
+    Button buttonSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, OrderDetailActivity.class);
+        initView();
+
+        buttonSignIn.setOnClickListener(l -> {
+            String nameUser = editTextName.getText().toString().trim();
+            String namePassword = editTextPassword.getText().toString().trim();
+
+            if(!(nameUser.isEmpty() || namePassword.isEmpty())) {
+                launchNextScreen(nameUser);
+            } else {
+                Toast.makeText(this,
+                        R.string.error_field_empty,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void launchNextScreen(String nameUser) {
+        Intent intent = MakeOrderActivity.newIntent(this, nameUser);
         startActivity(intent);
+    }
+
+    private void initView() {
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonSignIn = findViewById(R.id.buttonSignIn);
+        editTextName = findViewById(R.id.editTextName);
     }
 }
--- app/src/main/java/com/example/cafe/MakeOrderActivity.java
+++ app/src/main/java/com/example/cafe/MakeOrderActivity.java
@@ -2,7 +2,10 @@ package com.example.cafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MakeOrderActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MakeOrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_order);
+    }
+
+    public static Intent newIntent(Context context, String nameUser) {
+        Intent intent = new Intent(context, MakeOrderActivity.class);
+        intent.putExtra("nameUser", nameUser);
+        return intent;
     }
 }
